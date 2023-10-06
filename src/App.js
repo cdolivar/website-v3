c0038e2b5a4b82ddef6dfda65694921f674be77e
cdolivar <charles-david.olivar@priceline.com>
2023-10-05 21:46:37 -0400

    animate cursor

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React from "react";
 import { createGlobalStyle } from "styled-components";
 import styled from "styled-components";
 import AnimatedText from "./components/AnimatedText";
+import AnimatedCursor from "react-animated-cursor";
 
 const App = () => {
   const GlobalStyle = createGlobalStyle`
   body {
-    background: linear-gradient(90deg, hsla(217, 100%, 50%, 1) 0%, hsla(186, 100%, 69%, 1) 100%);
+    background-image: linear-gradient(135deg, rgb(102, 126, 234) 0%, rgb(118, 75, 162) 100%);
+    height:100vh;
+  }
+
+  html {
+    --cursor-color: #d5dedd
+  }
+  
+  html.dark-mode {
+    --cursor-color: #fff
   }
   `;
 
@@ -26,6 +36,20 @@ const App = () => {
   return (
     <>
       <GlobalStyle />
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: "var(--cursor-color)",
+        }}
+        outerStyle={{
+          border: "3px solid var(--cursor-color)",
+        }}
+      />
       <Container className="App">
         <headernpm className="App-header">
           <Text
