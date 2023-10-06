db9047258ef0aae9a0b389a074a4b856d092af16
cdolivar <charles-david.olivar@priceline.com>
2023-10-06 08:07:17 -0400

    remove scale

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ const App = () => {
   const Container = styled.div`
     text-align: left;
     font-size: 2em;
-    padding: 30px;
     max-width: 500px;
   `;
 
@@ -55,15 +54,15 @@ const App = () => {
       <Tilt
         style={{
           height: 300,
-
+          maxWidth: 400,
           width: "50%",
           margin: "0 auto",
         }}
         options={{
           reverse: true, // reverse the tilt direction
-          max: 90, // max tilt rotation (degrees)
+          max: 5, // max tilt rotation (degrees)
           perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
-          scale: 1.1, // 2 = 200%, 1.5 = 150%, etc..
+          scale: 1, // 2 = 200%, 1.5 = 150%, etc..
           speed: 1500, // Speed of the enter/exit transition
           transition: true, // Set a transition on enter/exit.
           reset: false, // If the tilt effect has to be reset on exit.
